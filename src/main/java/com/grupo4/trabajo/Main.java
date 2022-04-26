ec3603818f90f9d99643d5cfceef448be33f22e2
Bautista Carvalho <bautidenueve@gmail.com>
2022-04-26 19:10:32 -0300

    test

--- src/main/java/com/grupo4/trabajo/Main.java
+++ src/main/java/com/grupo4/trabajo/Main.java
@@ -5,6 +5,6 @@ import com.grupo4.trabajo.Servicios.Economic;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("sarasa");
+
     }
 }
---
+++ src/main/java/com/grupo4/trabajo/test.java
@@ -0,0 +1,4 @@
+package com.grupo4.trabajo;
+
+public class test {
+}
